4d15759bd8b440c2a444b34014cfa5a3d9c9bb7c
Patrick Rathje <git@patrickrathje.de>
2026-01-11 23:17:34 +0000

    publish 0.2.0

--- dist/Narration.js
+++
@@ -1,9 +0,0 @@
-export class Narration {
-    constructor(id, text, duration, audio) {
-        this.id = id;
-        this.text = text;
-        this.duration = duration; // the original duration of the given audio file
-        this.audio = audio;
-    }
-}
-//# sourceMappingURL=Narration.js.map
